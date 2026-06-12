1d1dedc35e28a9cb0f35e70b6018ed77b082bd18
sandeepsagar18 <sandeepsagarkumar85@gmail.com>
2026-06-12 15:09:38 +0530

    Initial commit

---
+++ src/pages/Agence.jsx
@@ -0,0 +1,120 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+
+const Agence = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+
+  const imageArray = [
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg",
+  ];
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: "top 28%",
+        end: "top -70%",
+        pin: true,
+        pinSpacing: true,
+        pinReparent: true,
+        pinType: "transform",
+        scrub: true, // smooth scrubbing with 1s easing
+        anticipatePin: 1,
+        invalidateOnRefresh: true,
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length);
+          } else {
+            imageIndex = imageArray.length - 1;
+          }
+          imageRef.current.src = imageArray[imageIndex];
+        },
+      },
+    });
+  });
+
+  return (
+    <div className="parent">
+      <div id="page1" className="py-1 ">
+        <div
+          ref={imageDivRef}
+          className="absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-96 -top-80 lg:left-[30vw] left-[30vw]"
+        >
+          <img
+            ref={imageRef}
+            className="h-full object-cover w-full"
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
+            alt=""
+          />
+        </div>
+        <div className="relative font-[font2]">
+          <div className="lg:mt-[55vh] mt-[30vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+              Soixan7e <br />
+              Douze
+            </h1>
+          </div>
+          <div className="lg:pl-[40%] lg:mt-20 mt-4 p-3">
+            <p className="lg:text-6xl text-xl leading-tight">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id="page2" className=" h-screen"></div>
+
+      <div className="px-20  h-full w-full bg-black py-20 flex flex-col gap-10">
+        <div className="relative overflow-hidden rounded-4xl">
+  <img
+    className="h-screen w-full object-cover transition-transform duration-500 hover:scale-110"
+    src="https://k72.ca/images/caseStudies/Opto/thumbnailimage_opto.jpg?w=1280&h=960&s=938f0bfb3de1ff2a2846b884eec2d757"
+    alt=""
+  />
+
+  <h2 className="absolute bottom-20 left-40 text-white uppercase text-6xl">
+    We see you like no other
+  </h2>
+</div>
+     <div className="relative overflow-hidden rounded-4xl">
+  <img
+    className="h-screen w-full object-cover transition-transform duration-500 hover:scale-110"
+    src="https://k72.ca/images/caseStudies/FRUITE/Fruite_thumbnail_bbq.jpg?w=1280&h=960&s=953c1f702bec28d66d07e95bc1261821"
+    alt=""
+  />
+
+  <h2 className="absolute bottom-40 left-60 text-white uppercase text-6xl">
+    Fruiti..
+  </h2>
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default Agence;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,74 @@
+import { useGSAP } from '@gsap/react'
+import ProjectCard from '../components/projects/ProjectCard'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+
+const Projects = () => {
+
+  const projects = [{
+    image1: 'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+  },{
+    image1:'https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93',
+
+    image2:'https://k72.ca/images/caseStudies/OKA/OKA_thumbnail.jpg?w=1280&h=960&s=c12c27c9db3c521e4c82a246a8d5c022'
+  },{
+    image1:'https://k72.ca/images/caseStudies/OKA/behance_OKA_1500X900_fromagetapisserie.jpg?w=1920&s=af6f4bf2e06adbdfb91a13b4651ba574',
+    image2:'https://k72.ca/uploads/caseStudies/OKA/arche.gif'
+  }
+]
+
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(function () {
+    gsap.from('.hero', {
+      height: '100px',
+      stagger: {
+        amount: 0.4
+      },
+      scrollTrigger: {
+        trigger: '.lol',
+        start: 'top 100%',
+        end: 'top -150%',
+        scrub: true
+      }
+    })
+  })
+
+  return (
+    <div className='lg:p-4 p-2 mb-[100vh]'>
+      <div className=' pt-[45vh]'>
+        <h2 className='font-[font2] lg:text-[9.5vw] text-7xl uppercase hover:text-yellow-300'>Projets</h2>
+      </div>
+      <div className='-lg:mt-20 lol'>
+        {projects.map(function (elem, idx) {
+          return <div key={idx} className='hero w-full lg:h-[850px] mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2'>
+            <ProjectCard image1={elem.image1} image2={elem.image2} />
+          
+          </div>
+
+
+        })}
+
+      </div>
+        <div className='h-full w-full '> <video autoPlay loop muted className='h-full w-full object-cover hover:rounded-2xl ' src="https://vod-progressive-ak.vimeocdn.com/exp=1781252222~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4727%2F27%2F698636736%2F3231249874.mp4~hmac=aa866cfa0fb2d9df4c72f63ee1465487efba6607021e13570c2b4f1ac46aaee8/vimeo-prod-skyfire-std-us/01/4727/27/698636736/3231249874.mp4"></video></div>
+      <footer className='bg-black h-[50vh] w-full hover:rounded-2xl absolute bottom-0  m-2   flex justify-evenly items-end py-10'> 
+        <h2 className='text-white uppercase hover:text-yellow-400 m-2 p-2  bottom-0'> privacy policy</h2>
+        <h2 className='text-white uppercase hover:text-yellow-400 m-2 p-2  bottom-0'> privacy notice</h2>
+        <h2 className='text-white uppercase hover:text-yellow-400 m-2 p-2  bottom-0'> ethics report</h2>
+        <h2 className='text-white uppercase hover:text-yellow-400 m-2 p-2  bottom-0'>consent choices</h2>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Projects
